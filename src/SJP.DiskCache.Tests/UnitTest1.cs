9c64fd1849e5cf86f3e6c669eb63f6dc04db874f
Simon Potter <simon@sjp.co.nz>
2017-08-30 20:41:12 -0400

    Add test project.

---
+++ src/SJP.DiskCache.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace SJP.DiskCache.Tests
+{
+    [TestFixture]
+    public class DiskCacheTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

0913105d7965961a67b6823723db2bedf96f2dab
Simon Potter <simon@sjp.co.nz>
2017-09-01 20:29:19 -0400

    Add unit tests for cache policies.

--- src/SJP.DiskCache.Tests/Policies/MfuCachePolicyTests.cs
+++ src/SJP.DiskCache.Tests/Policies/MfuCachePolicyTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Moq;
 using NUnit.Framework;
 
 namespace SJP.DiskCache.Tests
@@ -19,6 +20,87 @@ namespace SJP.DiskCache.Tests
         {
             var policy = new MfuCachePolicy<string>();
             Assert.Throws<ArgumentOutOfRangeException>(() => policy.GetExpiredEntries(Enumerable.Empty<ICacheEntry<string>>(), 0));
+        }
+
+        [Test]
+        public void GetExpiredEntries_WhenCacheNotExhausted_ReturnsEmptySet()
+        {
+            var policy = new MfuCachePolicy<string>();
+
+            var cacheEntry = new Mock<ICacheEntry<string>>();
+            cacheEntry.Setup(k => k.AccessCount).Returns(1);
+            cacheEntry.Setup(k => k.Size).Returns(1);
+            cacheEntry.Setup(k => k.Key).Returns("test");
+
+            var expired = policy.GetExpiredEntries(new[] { cacheEntry.Object }, 100).ToList();
+
+            Assert.AreEqual(0, expired.Count);
+        }
+
+        [Test]
+        public void GetExpiredEntries_WhenOnlyValueExceedsSize_ReturnsValue()
+        {
+            var policy = new MfuCachePolicy<string>();
+
+            var cacheEntry = new Mock<ICacheEntry<string>>();
+            cacheEntry.Setup(k => k.AccessCount).Returns(1);
+            cacheEntry.Setup(k => k.Size).Returns(101);
+            cacheEntry.Setup(k => k.Key).Returns("test");
+
+            var expired = policy.GetExpiredEntries(new[] { cacheEntry.Object }, 100).ToList();
+
+            Assert.AreEqual(1, expired.Count);
+        }
+
+        [Test]
+        public void GetExpiredEntries_WhenCacheExhausted_ReturnsNonEmptyCollection()
+        {
+            var policy = new MfuCachePolicy<string>();
+
+            var cacheEntry1 = new Mock<ICacheEntry<string>>();
+            cacheEntry1.Setup(k => k.AccessCount).Returns(1);
+            cacheEntry1.Setup(k => k.Size).Returns(5);
+            cacheEntry1.Setup(k => k.Key).Returns("test1");
+
+            var cacheEntry2 = new Mock<ICacheEntry<string>>();
+            cacheEntry2.Setup(k => k.AccessCount).Returns(2);
+            cacheEntry2.Setup(k => k.Size).Returns(5);
+            cacheEntry2.Setup(k => k.Key).Returns("test2");
+
+            var cacheEntry3 = new Mock<ICacheEntry<string>>();
+            cacheEntry3.Setup(k => k.AccessCount).Returns(3);
+            cacheEntry3.Setup(k => k.Size).Returns(5);
+            cacheEntry3.Setup(k => k.Key).Returns("test3");
+
+            var expired = policy.GetExpiredEntries(new[] { cacheEntry1.Object, cacheEntry2.Object, cacheEntry3.Object }, 12).ToList();
+
+            Assert.AreEqual(1, expired.Count);
+        }
+
+        [Test]
+        public void GetExpiredEntries_WhenCacheExhausted_ReturnsMostFrequentlyUsedValue()
+        {
+            var policy = new MfuCachePolicy<string>();
+
+            var cacheEntry1 = new Mock<ICacheEntry<string>>();
+            cacheEntry1.Setup(k => k.AccessCount).Returns(1);
+            cacheEntry1.Setup(k => k.Size).Returns(5);
+            cacheEntry1.Setup(k => k.Key).Returns("test1");
+
+            var cacheEntry2 = new Mock<ICacheEntry<string>>();
+            cacheEntry2.Setup(k => k.AccessCount).Returns(5);
+            cacheEntry2.Setup(k => k.Size).Returns(5);
+            cacheEntry2.Setup(k => k.Key).Returns("test2");
+
+            var cacheEntry3 = new Mock<ICacheEntry<string>>();
+            cacheEntry3.Setup(k => k.AccessCount).Returns(3);
+            cacheEntry3.Setup(k => k.Size).Returns(5);
+            cacheEntry3.Setup(k => k.Key).Returns("test3");
+
+            var expired = policy.GetExpiredEntries(new[] { cacheEntry1.Object, cacheEntry2.Object, cacheEntry3.Object }, 12).ToList();
+            var expiredKey = expired.Single().Key;
+
+            Assert.AreEqual(cacheEntry2.Object.Key, expiredKey);
         }
     }
 }
